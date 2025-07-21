511e29aba43f2d8a9d5770d3d9ba160d60668126
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-07-21 14:39:20 +0200

    chore: improve copy button (#68)

--- src/components/code-example.tsx
+++ src/components/code-example.tsx
@@ -14,6 +14,7 @@ import { highlightClasses } from "./highlight-classes";
 import atApplyInjection from "./syntax-highlighter/at-apply.json";
 import atRulesInjection from "./syntax-highlighter/at-rules.json";
 import themeFnInjection from "./syntax-highlighter/theme-fn.json";
+import { stripShikiComments } from "./shiki";
 
 export function js(strings: TemplateStringsArray, ...args: any[]) {
   return { lang: "js", code: dedent(strings, ...args) };
@@ -43,55 +44,6 @@ export function sh(strings: TemplateStringsArray, ...args: any[]) {
   return { lang: "sh", code: dedent(strings, ...args) };
 }
 
-export function unshiki(code: string): string {
-  const lines = code.split("\n");
-  const result: string[] = [];
-  let skip = 0;
-
-  const commentRegex = /\/\*.*?\*\/|\/\/.*|<!--.*?-->|#.*/g;
-  const codeTagRegex = /\[!code\s+([^\]]+)\]/;
-
-  for (let i = 0; i < lines.length; i++) {
-    // skip lines if a remove directive is active
-    if (skip > 0) {
-      skip--;
-      continue;
-    }
-
-    let line = lines[i];
-    const comments = [...line.matchAll(commentRegex)];
-
-    let removed = false;
-
-    // process comments to detect [!code ...] directives
-    for (const c of comments) {
-      const match = c[0].match(codeTagRegex);
-      if (match) {
-        // check if directive to remove next N lines
-        const spec = match[1];
-        const removeMatch = spec.match(/^--:(\d+)$/);
-        if (removeMatch) {
-          // set lines to skip
-          skip = parseInt(removeMatch[1], 10) - 1;
-          // current line removed (important if the line is not just a comment but also valid code)
-          removed = true;
-          break;
-        }
-
-        // remove comment if it's not a remove directive
-        line = line.slice(0, c.index) + line.slice(c.index! + c[0].length);
-      }
-    }
-
-    // add line if not removed and line is not empty or has no comments
-    if (!removed && (comments.length === 0 || line.trim() !== "")) {
-      result.push(line);
-    }
-  }
-
-  return result.join('\n').trim();
-}
-
 export async function CodeExample({
   example,
   filename,
@@ -103,11 +55,18 @@ export async function CodeExample({
 }) {
   return (
     <CodeExampleWrapper className={clsx('relative', className)}>
-      {filename ? <CodeExampleFilename filename={filename} /> : null}
-      <CopyButton
-        className="absolute right-4 top-2 z-10 text-stone-400"
-        value={example.code}
-      />
+      <div className="relative">
+        {filename ? <CodeExampleFilename filename={filename} /> : null}
+        <CopyButton
+          className={clsx(
+            "absolute z-10 transition duration-150 group-hover/code-block:opacity-100",
+            filename
+              ? "-top-1 right-0 text-white/50 hover:text-white/75"
+              : "top-2 right-2 rounded border border-black/15 bg-black/50 text-white/75 opacity-0 backdrop-blur-md hover:text-white",
+          )}
+          value={stripShikiComments(example.code)}
+        />
+      </div>
       <HighlightedCode example={example} className={example.className} />
     </CodeExampleWrapper>
   );
@@ -119,6 +78,7 @@ export function CodeExampleWrapper({ className, children }: { className?: string
       <div
         className={clsx(
           "rounded-xl p-1 text-sm scheme-dark in-data-stack:rounded-none dark:bg-white/5 dark:inset-ring dark:inset-ring-white/10 in-data-stack:dark:inset-ring-0",
+          "group/code-block",
           className,
         )}
       >
@@ -153,7 +113,7 @@ export function CodeExampleGroup({
         <div className="rounded-xl bg-gray-950 in-[figure]:-mx-1 in-[figure]:-mb-1">
           <div
             className={clsx(
-              "rounded-xl p-1 text-sm scheme-dark dark:bg-white/5 dark:inset-ring dark:inset-ring-white/10",
+              "relative rounded-xl p-1 text-sm scheme-dark dark:bg-white/5 dark:inset-ring dark:inset-ring-white/10",
               className,
             )}
           >
@@ -178,6 +138,13 @@ export function CodeExampleGroup({
 export function CodeBlock({ example }: { example: { lang: string; code: string } }) {
   return (
     <TabPanel>
+      <CopyButton
+        className={clsx(
+          "absolute z-10 transition duration-150 group-hover/code-block:opacity-100",
+          "top-0 right-0 z-10 text-white/50 hover:text-white/75",
+        )}
+        value={stripShikiComments(example.code)}
+      />
       <HighlightedCode example={example} />
     </TabPanel>
   );
@@ -249,7 +216,7 @@ function CodeExampleFilename({ filename }: { filename: string }) {
   return <div className="px-3 pt-0.5 pb-1.5 text-xs/5 text-gray-400 dark:text-white/50">{filename}</div>;
 }
 
-const highlighter = await createHighlighter({
+let highlighter = await createHighlighter({
   themes: [theme],
   langs: [
     atApplyInjection as any,
