8287ee2a991feac9907ffab8e3a24672413191bc
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-07-23 10:54:22 +0200

    feat: gatsby tailwindcss guide

---
+++ src/app/(docs)/docs/tailwindcss/installation/framework-guides/gatsby.tsx
@@ -0,0 +1,16 @@
+import { type Page, type Tile } from "./utils";
+import Logo from "@/docs/img/guides/ember.react.svg";
+
+export let tile: Tile = {
+  title: "Gatsby",
+  description: "Framework for building static sites with React and GraphQL.",
+  type: "official",
+  external: "https://tailwindcss.com/docs/installation/framework-guides/emberjs",
+
+  Logo,
+};
+
+export let page: Page = {
+  title: "Install Tailwind CSS with Gatsby",
+  description: "Setting up Tailwind CSS in a Gatsby project.",
+};
