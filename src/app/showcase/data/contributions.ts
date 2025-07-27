ac8424422a26c4474eff3a3667fc5df955b701e7
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-07-27 22:37:02 +0200

    Create contributions.ts

---
+++ src/app/showcase/data/contributions.ts
@@ -0,0 +1,309 @@
+// Auto-generated contributions data
+const contributions = [
+  {
+    name: "tailwindlabs/tailwindcss.com",
+    stars: 3588,
+    prs: 6,
+    issues: 10,
+    discussions: 0,
+    comments: 28
+  },
+  {
+    name: "tailwindlabs/tailwindcss",
+    stars: 89181,
+    prs: 2,
+    issues: 7,
+    discussions: 19,
+    comments: 117
+  },
+  {
+    name: "fatihsolhan/v-onboarding",
+    stars: 195,
+    prs: 6,
+    issues: 0,
+    discussions: 0,
+    comments: 11
+  },
+  {
+    name: "xiCO2k/laravel-vue-i18n",
+    stars: 673,
+    prs: 2,
+    issues: 1,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "vercel/next.js",
+    stars: 133389,
+    prs: 1,
+    issues: 1,
+    discussions: 0,
+    comments: 3
+  },
+  {
+    name: "saadeghi/daisyui",
+    stars: 37995,
+    prs: 1,
+    issues: 1,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "primefaces/primevue",
+    stars: 13081,
+    prs: 0,
+    issues: 2,
+    discussions: 0,
+    comments: 2
+  },
+  {
+    name: "googleapis/release-please",
+    stars: 5676,
+    prs: 1,
+    issues: 1,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "laravel/docs",
+    stars: 3185,
+    prs: 2,
+    issues: 0,
+    discussions: 0,
+    comments: 2
+  },
+  {
+    name: "antares-sql/antares",
+    stars: 2292,
+    prs: 0,
+    issues: 2,
+    discussions: 0,
+    comments: 2
+  },
+  {
+    name: "googleapis/release-please-action",
+    stars: 2018,
+    prs: 2,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "laravel/pulse",
+    stars: 1595,
+    prs: 2,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "aarondfrancis/pulse-outdated",
+    stars: 92,
+    prs: 1,
+    issues: 1,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "microsoft/vscode",
+    stars: 174927,
+    prs: 0,
+    issues: 1,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "laravel/laravel",
+    stars: 81597,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "laravel/framework",
+    stars: 33874,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 3
+  },
+  {
+    name: "heroui-inc/heroui",
+    stars: 24947,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "unocss/unocss",
+    stars: 17914,
+    prs: 0,
+    issues: 1,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "unplugin/unplugin-auto-import",
+    stars: 3590,
+    prs: 0,
+    issues: 1,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "tailwindlabs/tailwindcss-intellisense",
+    stars: 3207,
+    prs: 0,
+    issues: 1,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "smastrom/notivue",
+    stars: 837,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "cssninjaStudio/unplugin-fonts",
+    stars: 392,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "laravel/livewire-starter-kit",
+    stars: 353,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "n4j1Br4ch1D/postcss-obfuscator",
+    stars: 174,
+    prs: 0,
+    issues: 1,
+    discussions: 0,
+    comments: 0
+  },
+  {
+    name: "TheSeally/vite-plugin-pug-transformer",
+    stars: 39,
+    prs: 1,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "shadcn-ui/ui",
+    stars: 91571,
+    prs: 0,
+    issues: 0,
+    discussions: 1,
+    comments: 0
+  },
+  {
+    name: "apache/echarts",
+    stars: 64057,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 3
+  },
+  {
+    name: "tailwindlabs/headlessui",
+    stars: 27686,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "jdx/mise",
+    stars: 17799,
+    prs: 0,
+    issues: 0,
+    discussions: 1,
+    comments: 1
+  },
+  {
+    name: "nuxt/ui",
+    stars: 5116,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "primer/github-vscode-theme",
+    stars: 1979,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "beholdr/maska",
+    stars: 1923,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 7
+  },
+  {
+    name: "laravel/vs-code-extension",
+    stars: 739,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "microsoft/TypeScript-DOM-lib-generator",
+    stars: 678,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "lepikhinb/laravel-typescript",
+    stars: 398,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "heroui-inc/next-app-template",
+    stars: 358,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "anish2690/vue-color-kit",
+    stars: 215,
+    prs: 0,
+    issues: 0,
+    discussions: 0,
+    comments: 1
+  },
+  {
+    name: "dobicinaitis/tailwind-cli-extra",
+    stars: 141,
+    prs: 0,
+    issues: 0,
+    discussions: 1,
+    comments: 0
+  }
+];
+
+export default contributions;
