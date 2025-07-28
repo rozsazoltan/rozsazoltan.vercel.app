42defa5b0736105651ad8e46588a66d8d77e2adb
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-07-28 15:26:45 +0200

    Create stackoverflow.ts

---
+++ src/app/showcase/data/stackoverflow.ts
@@ -0,0 +1,14 @@
+// Auto-generated contributions data
+const stackoverflow = {
+  answers: 690,
+  reputation: 15140,
+  views: "0.9M",
+  tags: ["javascript", "reactjs", "vue.js", "vite", "tailwind-css", "typescript", "php", "laravel", "next.js", "visual-studio-code", "angular"],
+  badges: {
+    gold: 7,
+    silver: 32,
+    bronze: 98
+  }
+};
+
+export default stackoverflow;
