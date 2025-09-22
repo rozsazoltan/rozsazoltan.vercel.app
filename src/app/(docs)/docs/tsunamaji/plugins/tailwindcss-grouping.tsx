f793817b1117552ae0007ed402eec99e66a64bd6
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-09-22 11:42:08 +0200

    note tailwindcss-grouping coming soon

--- src/app/(docs)/docs/tsunamaji/plugins/tailwindcss-grouping.tsx
+++ src/app/(docs)/docs/tsunamaji/plugins/tailwindcss-grouping.tsx
@@ -1,136 +1,51 @@
-import { Step } from "./utils";
+import { Step, Tab } from "./utils";
 import { type Page, type Tile } from "./utils";
-import Logo from "@/docs/img/guides/vite.react.svg";
+import Logo from "@/docs/img/guides/tsunamaji.react.svg";
 import dedent from "dedent";
 
 export let tile: Tile = {
-  title: "Vite",
-  description: "Blazing-fast build tool with native Tailwind support and instant updates.",
+  title: "Tailwind Grouping",
+  description: "Drop the variant repetition and group them without any additional drawbacks.",
 
   Logo,
 };
 
 export let page: Page = {
-  title: "Install Tailwind CSS with Vite.js plugin",
-  description: "Installing Tailwind CSS as a Vite plugin is the most seamless way to integrate it with frameworks like Laravel, SvelteKit, React Router, Nuxt, and SolidJS.",
+  title: "Install Tailwind Grouping (by Tsunamaji)",
+  description: "The plugin's purpose is to consolidate variant repetitions. For example, instead of writing `lg:p-12 lg:bg-emerald-300 lg:text-emerald-500 lg:dark:bg-emerald-800 lg:dark:text-emerald-500`, it enforces the following: `lg:(p-12,bg-emerald-300,text-emerald-500,dark:(bg-emerald-800,text-emerald-500))`. In general, this would result in larger generated CSS, but with our plugin, it does not.",
 };
+
+export let tabs: Tab[] = [
+  {
+    slug: "node",
+    title: "Using Node.js",
+  },
+  {
+    slug: "play-esm",
+    title: "Play ESM (esm.sh)",
+  },
+  {
+    slug: "play-cdn",
+    title: "Play CDN",
+  },
+];
 
 export let steps: Step[] = [
   {
-    title: "Create your project",
+    title: "Coming soon",
     body: (
       <p>
-        Start by creating a new Vite project if you don't have one set up already. The most common approach is to use{" "}
-        <a href="https://vite.dev/guide/#scaffolding-your-first-vite-project">Create Vite</a>.
+        The plugin is under development.
       </p>
     ),
     code: {
       name: "Terminal",
       lang: "shell",
       code: dedent`
-        npm create vite@latest my-project
-        cd my-project
-      `,
-    },
-  },
-  {
-    title: "Install Tailwind CSS",
-    body: (
-      <p>
-        Install <code>tailwindcss</code> and <code>@tailwindcss/vite</code> via npm.
-      </p>
-    ),
-    code: {
-      name: "Terminal",
-      lang: "shell",
-      code: dedent`
-        npm install tailwindcss @tailwindcss/vite
-      `,
-    },
-  },
-  {
-    title: "Configure the Vite plugin",
-    body: (
-      <p>
-        Add the <code>@tailwindcss/vite</code> plugin to your Vite configuration.
-      </p>
-    ),
-    code: {
-      name: "vite.config.ts",
-      lang: "js",
-      code: dedent`
-        import { defineConfig } from 'vite'
-        // [!code highlight:2]
-        import tailwindcss from '@tailwindcss/vite'
-
-        export default defineConfig({
-          plugins: [
-            // [!code highlight:2]
-            tailwindcss(),
-          ],
-        })
-      `,
-    },
-  },
-  {
-    title: "Import Tailwind CSS",
-    body: (
-      <p>
-        Add an <code>@import</code> to your CSS file that imports Tailwind CSS.
-      </p>
-    ),
-    code: {
-      name: "CSS",
-      lang: "css",
-      code: dedent`
-        @import "tailwindcss";
-      `,
-    },
-  },
-  {
-    title: "Start your build process",
-    body: (
-      <p>
-        Run your build process with <code>npm run dev</code> or whatever command is configured in your{" "}
-        <code>package.json</code> file.
-      </p>
-    ),
-    code: {
-      name: "Terminal",
-      lang: "shell",
-      code: dedent`
-        npm run dev
-      `,
-    },
-  },
-  {
-    title: "Start using Tailwind in your HTML",
-    body: (
-      <p>
-        Make sure your compiled CSS is included in the <code>{"<head>"}</code>{" "}
-        <em>(your framework might handle this for you)</em>, then start using Tailwind’s utility classes to style your
-        content.
-      </p>
-    ),
-    code: {
-      name: "HTML",
-      lang: "html",
-      code: dedent`
-        <!doctype html>
-        <html>
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <!-- [!code highlight:2] -->
-          <link href="/src/style.css" rel="stylesheet">
-        </head>
-        <body>
-          <!-- [!code highlight:4] -->
-          <h1 class="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-        </body>
-        </html>
+        check-plugin-status
+        
+        > [PluginLoader] ⚠️ Warning: Plugin not ready yet!
+        > Status: 🐢 Slow-cooking in the dev kitchen...
       `,
     },
   },
